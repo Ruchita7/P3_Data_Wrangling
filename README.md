b5987688649d5192fb05249e0ec92d6d1169f1ae
Ruchita7 <ruchita.maheshwary@gmail.com>
2017-02-16 09:35:43 +0530

    refractor : format code and report

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# P3_Data_Wrangling
+#P3- Data Wrangling with Mongo DB
+##(Open Street Map)
+
+####Map Area: New York City, United States of America
+http://www.openstreetmap.org/relation/175905#map=10/40.6978/-73.9792
+
+ 
+I chose **New York City** as my data extract for P3-Data Wrangling with Mongo DB. Though I have never been there but I chose this city as I am in completely awe of that place due to its cosmopolitan,bustling hive of activity nature and also it being melting pot of diverse cultures/ethnicity. It is also home to various famous places like central park, times square,statue of liberty, madison square garden and metropolitan Museum of Art which have been captured beautifully in many popular movies and shows. Also about "The Big Apple", I have read that it's a shopper’s paradise and is famous for its multi cuisine nature which excites me as a future traveller.
+
+So for these reasons I chose New York City as my data set. 
+I downloaded the data via a custom extract from Mapzen (https://mapzen.com/data/metro-extracts/your-extracts/079ffc8778d7)
--- process_data.py
+++ process_data.py
@@ -5,165 +5,179 @@ import pprint
 import codecs
 import json
 
-#regular expression for lower case characters
+# regular expression for lower case characters
 lower = re.compile(r'^([a-z]|_)*$')
-#regular expression for lower case characters containing colon
+# regular expression for lower case characters containing colon
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
-#regular expression for problem characters
+# regular expression for problem characters
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
-#list for created values
-CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
+# list for created values
+CREATED = ["version", "changeset", "timestamp", "user", "uid"]
 
-#regular expression for street type
+# regular expression for street type
 street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 
 postcode_re = re.compile(r"^[0-9]{5}$", re.IGNORECASE)
 
-#list for expected street types
-expected = ["Avenue","Boulevard","Common","Concourse","Circle","Crescent","Court","Center",
-            "Drive","East","Heights","Interstate","Lane","Place","North","North West","Parkway",
-            "Plaza","Road","Ridge", "Square","Street", "South","South East", "Trail","West"]
+# list for expected street types
+expected = ["Avenue", "Boulevard", "Common", "Concourse", "Circle", "Crescent", "Court", "Center",
+            "Drive", "East", "Heights", "Interstate", "Lane", "Place", "North", "North West", "Parkway",
+            "Plaza", "Road", "Ridge", "Square", "Street", "South", "South East", "Trail", "West"]
 
-#dictionary for invalid street type mappings
+# dictionary for invalid street type mappings
 mapping = {
-            "Ave": "Avenue","Ave.": "Avenue","ave": "Avenue",
-            "Blvd": "Boulevard","Blvd.": "Boulevard",
-            "Com": "Common","Com.": "Common",
-            "Concrs": "Concourse","Concrs.": "Concourse",
-            "Cir": "Circle","Cir.": "Circle",
-            "Cres": "Crescent","Cres.": "Crescent",
-            "Ct.": "Court","Ct": "Court",
-            "Ctr": "Center","Ctr.": "Center",    
-            "Dr": "Drive","Dr.": "Drive",
-            "E": "East",
-            "Hts": "Heights","Hts.": "Heights",
-            "I" : "Interstate",
-            "Ln": "Lane","Ln.": "Lane",
-            "Pl": "Place","Pl.": "Place",
-            "Plz": "Plaza","Plz.": "Plaza",
-            "N":"North",
-            "NW":"North West",
-            "Pkwy": "Parkway","Pkwy.": "Parkway",
-            "Rd": "Road","Rd.": "Road",
-             "Rdg": "Ridge","Rdg.": "Ridge",
-            "S":"South",
-            "SE": "South East",
-            "Sq": "Square","Sq.": "Square",
-            "St": "Street","St.": "Street","st": "Street", 
-            "Trl" : "Trail","Trl." : "Trail",
-            "W":"West"
-            }
+    "Ave": "Avenue", "Ave.": "Avenue", "ave": "Avenue",
+    "Blvd": "Boulevard", "Blvd.": "Boulevard",
+    "Com": "Common", "Com.": "Common",
+    "Concrs": "Concourse", "Concrs.": "Concourse",
+    "Cir": "Circle", "Cir.": "Circle",
+    "Cres": "Crescent", "Cres.": "Crescent",
+    "Ct.": "Court", "Ct": "Court",
+    "Ctr": "Center", "Ctr.": "Center",
+    "Dr": "Drive", "Dr.": "Drive",
+    "E": "East",
+    "Hts": "Heights", "Hts.": "Heights",
+    "I": "Interstate",
+    "Ln": "Lane", "Ln.": "Lane",
+    "Pl": "Place", "Pl.": "Place",
+    "Plz": "Plaza", "Plz.": "Plaza",
+    "N": "North",
+    "NW": "North West",
+    "Pkwy": "Parkway", "Pkwy.": "Parkway",
+    "Rd": "Road", "Rd.": "Road",
+    "Rdg": "Ridge", "Rdg.": "Ridge",
+    "S": "South",
+    "SE": "South East",
+    "Sq": "Square", "Sq.": "Square",
+    "St": "Street", "St.": "Street", "st": "Street",
+    "Trl": "Trail", "Trl.": "Trail",
+    "W": "West"
+}
+
 
 def is_ascii(str):
     return all(ord(chars) < 128 for chars in str)
 
-#match post code
+
+# match post code
 def match_postcode(zip):
     m = postcode_re.match(zip)
     if m:
         return True
     return False
-        
-#update street/city names if match found
+
+
+# update street/city names if match found
 def update_name(name, mapping):
-    #find street names if matching street_type
+    # find street names if matching street_type
     address_name = name.split();
-    for st in address_name :
+    for st in address_name:
         m = street_type_re.search(st)
         if m:
             street_type = m.group()
-        #check if street_type is key in mapping, if so substitute mapping
+            # check if street_type is key in mapping, if so substitute mapping
             if street_type in mapping.keys():
-                name = re.sub(street_type, mapping[street_type],name)
+                name = re.sub(street_type, mapping[street_type], name)
     return name
 
+#return latitude/longitude
+def find_positions(element):
+    pos = []
+    pos.append(float(element.attrib["lat"]))
+    pos.append(float(element.attrib["lon"]))
+    return pos
+
+#retrieve mapping for address field
+def retrieveAddress(child,elementTag):
+    if len(elementTag) == 2:
+        if child.attrib['k'] == "addr:street" or child.attrib['k'] == "addr:city":
+            return update_name(child.attrib['v'], mapping)
+        elif elementTag[1] == "postcode":
+            if match_postcode(child.attrib['v']) is False:
+                pass
+            else:
+                return child.attrib['v']
+        # process other address tags of level two
+        else:
+                return child.attrib['v']
+
 def shape_element(element):
-    node = {}   #dictionary for tag
-    node_refs = []  #list for node references
-    created = {}    #dictionary for created
-    addr = {}       #dictionary for address
-    otherKeys = {}  #dictionary for other tags
+    node = {}  # dictionary for tag
+    node_refs = []  # list for node references
+    created = {}  # dictionary for created
+    addr = {}  # dictionary for address
+    otherKeys = {}  # dictionary for other tags
     if element.tag == "node" or element.tag == "way":
         node["type"] = element.tag
         for key in element.attrib.keys():
-            #check if attribute is among created attributes
+            value = element.attrib[key]
+            # check if attribute is among created attributes
             if key in CREATED:
-                created[key] = element.attrib[key]
-            #add latitude/longitude attributes to array "pos"
-            elif (key in "lat" or key in "lon"):
-                pos = []
-                pos.append(float(element.attrib["lat"]))
-                pos.append(float(element.attrib["lon"]))
-                node["pos"] = pos
+                created[key] = value
+            # add latitude/longitude attributes to array "pos"
+            elif key in "lat" or key in "lon":
+                node["pos"] = find_positions(element)
             else:
-                #add other attributes as it is
-                node[key] = element.attrib[key]
-            #created dictionary
+                # add other attributes as it is
+                node[key] = value
+            # created dictionary
             node["created"] = created
-        #iterate child tag elements
+        # iterate child tag elements
         for child in element:
-            #if attribute tag is "nd"
+            # if attribute tag is "nd"
             if child.tag == "nd":
                 node_refs.append(child.attrib["ref"])
-            #determine if attribute key/value is valid
+            # determine if attribute key/value is valid
             elif is_valid(child):
-                #handle street values and update if needed
+                # handle street values and update if needed
                 elementTag = child.attrib['k'].split(":")
-                #update mapping for street or city
-                if child.attrib['k']=="addr:street" or child.attrib['k']=="addr:city":
-                        addr[elementTag[1]]=update_name(child.attrib['v'],mapping)
-                    
-                #process other address tags of level two
-                elif child.attrib['k'].startswith("addr:"):
-                    if len(elementTag) == 2:
-                        if elementTag[1]=="postcode":
-                            if match_postcode(child.attrib['v'])==False:
-                                pass
-                        address_tag = elementTag[1]
-                        addr[address_tag] = child.attrib['v']
-                #process other tag attributes
-                else:                 
-                    #if attribute does not contain ':' put it as key/value pair
+                if child.attrib['k'].startswith("addr:"):
+                    addr[elementTag[1]] = retrieveAddress(child,elementTag)
+                # process other tag attributes
+                else:
+                    # if attribute does not contain ':' put it as key/value pair
                     if len(elementTag) <= 1:
                         node[child.attrib['k']] = child.attrib['v']
-                    #if attribute contains colon then process only second level tags
+                    # if attribute contains colon then process only second level tags
                     elif len(elementTag) == 2:
                         otherAttribute = elementTag[0]
-                        #if tag name already not added to 'node' dictionary then add it as dictionary
+                        # if tag name already not added to 'node' dictionary then add it as dictionary
                         if not node.has_key(otherAttribute):
                             otherKeys = {}
                         else:
                             obj = node.get(otherAttribute)
                             # determine object type if is string and already present, then set it as dictionary in 'otherKeys'
-                            if (type(obj) is str):
+                            if type(obj) is str:
                                 otherKeys = {}
                                 otherKeys[otherAttribute] = obj
                             del node[otherAttribute]
-                        #add the new key/value pair to 'otherKeys' dictionary and and add to node
+                        # add the new key/value pair to 'otherKeys' dictionary and and add to node
                         otherKeys[elementTag[1]] = child.attrib['v']
                         node[otherAttribute] = otherKeys
-        #check if node_refs,addr have length then add to node dictionary
-        if (len(node_refs) > 0):
+        # check if node_refs,addr have length then add to node dictionary
+        if len(node_refs) > 0:
             node["node_refs"] = node_refs
-        if (len(addr) > 0):
+        if len(addr) > 0:
             node["address"] = addr
         return node
     else:
         return None
 
-#check if key is valid or not
+
+# check if key is valid or not
 def is_valid(element):
-    l=lower.search(element.attrib['k'])
-    lc=lower_colon.search(element.attrib['k'])
-    pc=problemchars.search(element.attrib['k'])
-    #if problem characters found return false
-    if pc or is_ascii(element.attrib['v'])==False:
+    l = lower.search(element.attrib['k'])
+    lc = lower_colon.search(element.attrib['k'])
+    pc = problemchars.search(element.attrib['k'])
+    # if problem characters found return false
+    if pc or is_ascii(element.attrib['v']) == False:
         return False
     else:
         return True
 
-#process osm file
+
+# process osm file
 def process_data(file_in, pretty=False):
     # You do not need to change this file
     file_out = "{0}.json".format(file_in)
@@ -181,8 +195,9 @@ def process_data(file_in, pretty=False):
 
 
 def test():
-    data = process_data('new_york_sample.osm', False)
-    #pprint.pprint(data)
+    data = process_data('new_york_sample_small.osm', False)
+    # pprint.pprint(data)
+
 
 if __name__ == "__main__":
     test()
